64a4e96cb9481fb8f252113f75db1278c797f152
Hannes Rüger <hannesrueger@gmx.de>
2023-03-25 16:50:31 +0100

    update pages

---
+++ update-config.mjs
@@ -0,0 +1,45 @@
+import fs from "fs";
+import yaml from "yaml";
+
+const FILENAME = ".upptimerc.yml"
+const globalSites = {
+    "Dein-Ticket.Shop": "https://dein-ticket.shop",
+    "Login": "https://login.dein-ticket.shop",
+};
+
+const organizations = {
+    "klecks": "Kleinkunstverein Klecks e.V.",
+    "kqa": "Kunstquartier Allgäu",
+    "alpe-muellers-berg": "Alpe Müllers Berg",
+};
+
+const subdomains = {
+    "": "Verkaufsseite",
+    "admin": "Admin-Bereich",
+    "vvk": "Vorverkauf",
+};
+
+const content = fs.readFileSync(FILENAME, "utf8");
+const data = yaml.parse(content);
+
+data.sites = [];
+
+for (const [key, value] of Object.entries(globalSites)) {
+    data.sites.push({
+        name: key,
+        url: value,
+    });
+}
+
+for (const [key, value] of Object.entries(organizations)) {
+    for (const [subdomain, subdomainName] of Object.entries(subdomains)) {
+        const name = `${value} - ${subdomainName}`;
+        const url = `https://${subdomain}.${key}.dein-ticket.shop`;
+        data.sites.push({
+            name,
+            url,
+        });
+    }
+}
+
+fs.writeFileSync(FILENAME, yaml.stringify(data));
